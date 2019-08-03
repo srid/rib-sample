7c564396503c77fb7036636d4284589968bfb288
Sridhar Ratnakumar <srid@srid.ca>
2019-08-03 17:13:07 -0400

    Simplify default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,14 @@
-let
-  rib = (import <nixpkgs> {}).fetchFromGitHub {
+{ pkgs ? import <nixpkgs> {}
+# Cabal project root
+, root ? ./. 
+# Rib library source to use
+, rib ? pkgs.fetchFromGitHub {
     owner = "srid";
     repo = "rib";
     rev = "25875b4af22833bf8e6debdd136a3c56d5fd04b4";
     sha256 = "0x9blxpvnl02hj76msli60ffbliqrl23v6gp59rsvi9qgyilcfmc";
-  };
-in
-import (rib + /default.nix) {
-  root = ./.;
-}
+  }
+, ...
+}:
+
+import rib { inherit pkgs root; }
