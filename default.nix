d2f21129c988e11a285d8f42d992de78e2a724f5
Sridhar Ratnakumar <srid@srid.ca>
2019-08-07 17:44:35 -0400

    Update rib

--- default.nix
+++ default.nix
@@ -1,14 +1,11 @@
 { pkgs ? import <nixpkgs> {}
 # Cabal project root
-, root ? ./. 
+, root ? ./.
+# Cabal project name
+, name ? "rib-sample"
 # Rib library source to use
-, rib ? pkgs.fetchFromGitHub {
-    owner = "srid";
-    repo = "rib";
-    rev = "25875b4af22833bf8e6debdd136a3c56d5fd04b4";
-    sha256 = "0x9blxpvnl02hj76msli60ffbliqrl23v6gp59rsvi9qgyilcfmc";
-  }
+, rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/3f6dc48.tar.gz"
 , ...
 }:
 
-import rib { inherit pkgs root; }
+import rib { inherit pkgs root name; }
