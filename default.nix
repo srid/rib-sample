7469b4130b8168132d3abfea2373aaf60b1e91d9
Sridhar Ratnakumar <srid@srid.ca>
2019-08-07 17:46:47 -0400

    Re-use rib's nixpkgs

--- default.nix
+++ default.nix
@@ -1,11 +1,11 @@
-{ pkgs ? import <nixpkgs> {}
+{
+# Rib library source to use
+  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/3f6dc48.tar.gz"
 # Cabal project root
 , root ? ./.
 # Cabal project name
 , name ? "rib-sample"
-# Rib library source to use
-, rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/3f6dc48.tar.gz"
 , ...
 }:
 
-import rib { inherit pkgs root name; }
+import rib { inherit root name; }
