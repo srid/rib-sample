f5dff7df63d3b2fb3401f6b58c5a7156084ac5a8
Sridhar Ratnakumar <srid@srid.ca>
2019-08-25 12:24:46 -0400

    Update rib, so cabal-install buildTool is available

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {
 # Rib library source to use
-  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/3f6dc48.tar.gz"
+  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/18db6cc.tar.gz"
 # Cabal project root
 , root ? ./.
 # Cabal project name
