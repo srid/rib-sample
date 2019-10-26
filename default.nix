cf726e6a885109158846833cf21871062675067a
Sridhar Ratnakumar <srid@srid.ca>
2019-10-26 11:45:54 -0400

    Fix incorrect rib hash in default.nix
    Ref: parent commit

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {
 # Rib library source to use
-  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/e6a6742.tar.gz"
+  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/4dbbeff.tar.gz"
 # Cabal project root
 , root ? ./.
 # Cabal project name
