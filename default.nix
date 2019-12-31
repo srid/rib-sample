ca00750266031323526895db08185838df378d5e
Sridhar Ratnakumar <srid@srid.ca>
2019-12-31 11:04:49 -0500

    Add CI (#5); document nix cache

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {
 # Rib library source to use
-  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/11a9f5d.tar.gz"
+  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/eadcdfd.tar.gz"
 # Cabal project root
 , root ? ./.
 # Cabal project name
