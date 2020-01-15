981b1a242904d89a2c502241448dd09b2037e343
Sridhar Ratnakumar <srid@srid.ca>
2020-01-15 09:55:03 -0500

    Advance rib

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "03510616ab2e64d58b1d87b69ea357ab2b3b79ed";
+  ribRevision = "40cc8d7";
 in {
 # Rib library source to use
   rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
