ab6644221fa9abf57b263de3f607c3766efb3814
Sridhar Ratnakumar <srid@srid.ca>
2020-01-21 13:02:46 -0500

    Use nix gitignore to speedup nix builds
    Also advance rib

--- default.nix
+++ default.nix
@@ -2,12 +2,14 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "40cc8d7";
+  ribRevision = "1a1470534f84f23457931ef9e4305d71834881f9";
+
+  inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
 in {
 # Rib library source to use
   rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
 # Cabal project root
-, root ? ./.
+, root ? gitignoreSource ./.
 # Cabal project name
 , name ? "rib-sample"
 , ...
