68e139c979ac2d2d34eeb75958f9cf39f842d4c1
Sridhar Ratnakumar <srid@srid.ca>
2020-05-11 21:33:48 -0400

    Advance rib
    (cherry picked from commit 37ca1c6875512c7a10f0243d2a9564b6f78e515c)

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@ let
   # To upgrade rib, go to https://github.com/srid/rib/commits/master, select the
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
-  ribRevision = "157f466";
+  ribRevision = "4b3d374aec7cc16fc046b46bbac7eb577f326696";
 
   inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
 in {
@@ -17,7 +17,6 @@ in {
 
 let 
   source-overrides = {
-    with-utf8 = builtins.fetchTarball "https://github.com/serokell/haskell-with-utf8/archive/v1.0.0.0.tar.gz";
   };
 in import rib { 
   inherit root name source-overrides; 
