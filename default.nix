a28d0e31aea95d53ff4736bf596ca529096c2cf6
Sridhar Ratnakumar <srid@srid.ca>
2020-09-16 11:06:37 -0400

    nix: specify sha256 to prevent refetches

--- default.nix
+++ default.nix
@@ -3,11 +3,19 @@ let
   # revision you would like to upgrade to and set it here. Consult rib's
   # ChangeLog.md to check any notes on API migration.
   ribRevision = "42bc7c3";
+  ribSrc = builtins.fetchTarball {
+    url = "https://github.com/srid/rib/archive/${ribRevision}.tar.gz";
+    sha256 = "1dph2nar6wfv9krkpcg6imdfjz3vzhwm5624x9kqbslsgvd9lcxx";
+  };
 
-  inherit (import (builtins.fetchTarball "https://github.com/hercules-ci/gitignore/archive/7415c4f.tar.gz") { }) gitignoreSource;
+  gitignoreSrc = builtins.fetchTarball { 
+    url = "https://github.com/hercules-ci/gitignore/archive/c4662e6.tar.gz";
+    sha256 = "1npnx0h6bd0d7ql93ka7azhj40zgjp815fw2r6smg8ch9p7mzdlx";
+  };
+  inherit (import gitignoreSrc { }) gitignoreSource;
 in {
 # Rib library source to use
-  rib ? builtins.fetchTarball "https://github.com/srid/rib/archive/${ribRevision}.tar.gz"
+  rib ? ribSrc
 # Cabal project root
 , root ? gitignoreSource ./.
 # Cabal project name
@@ -16,6 +24,7 @@ in {
 }:
 
 let 
+  # Add your Haskell package overrides here
   source-overrides = {
   };
 in import rib { 
